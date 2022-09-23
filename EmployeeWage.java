5472fa531e556386d9dd09d9f46e144ea5993d88
Shiv25z <shivamzagade25899@gmail.com>
2022-09-23 13:24:17 +0530

    Added UC2: Calculation of daily wage of employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -10,17 +10,20 @@ public class EmployeeWage {
         Random r = new Random();
         Scanner sc =new Scanner(System.in);
         int n = r.nextInt(2);
-
+        int wagePerHour = 20;
         int empHour ;
+
         switch(n){ // UC1 to check the Employee is present or absent using RANDOM class.
             case 0:
-
-                System.out.println("Employee is Present  ");
+                empHour=8;
+                System.out.println("Employee is Present for "+empHour+" hours. ");
                 break;
 
             default:
                 empHour=0;
                 System.out.println("Employee is Absent so no salary for him...");
         }
+        int wage = wagePerHour*empHour; // UC2 to calculate the daily wage for present employee
+        System.out.println("Employee Wage for one day is: "+wage);
     }
 }
