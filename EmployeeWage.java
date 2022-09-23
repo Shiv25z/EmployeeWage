0ea546ff83e3e1d11c373a1d976ca84095e7009f
Shiv25z <shivamzagade25899@gmail.com>
2022-09-23 13:42:33 +0530

    Added UC6: Check if employee mets working hour policy

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -36,6 +36,16 @@ public class EmployeeWage {
         int monthlyHour = empHour*workDays;
         System.out.println("Employee this month salary will be : "+monthlyWage);
 
+        System.out.println("");//uc6 to check if the Employee working hours or days meets the company requirements
+        System.out.println(""); // i.e. working days atleast should be 20 in a month or minimum of 100 working hours
+        if (workDays >=20 || monthlyHour >=100){
+            System.out.println("Worker Satisfies the payscale condition.");
+            System.out.println("The Final Salary will be credited to Workers  bank account is: "+monthlyWage);
+        }
+        else{
+            System.out.println("Worker did not satisfies the payscale condition.");
+            System.out.println("Worker needs to work for more hour or minimum for 20 days. ");
+        }
 
 
 
